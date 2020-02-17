5ba419a5aeef56712516d5ce99700021e8ec99ae
Micah Lund <micah.r.lund@gmail.com>
2020-02-16 19:35:56 -0700

    Added if statements to convert month number to month name, however, the month name is not rendering.

--- main.js
+++ main.js
@@ -10,9 +10,32 @@ console.log(
 );
 
 //If or Switch statement for displaying the actual month name versus the month number. 
-// if date.getMonth = 1 {
-//     return "January"
-// }
+if (date.getMonth = 1) {
+    return "January"
+} if (date.getMonth = 2) {
+    return "February"
+} if (date.getMonth = 3) {
+    return "March"
+} if (date.getMonth = 4) {
+    return "April"
+} if (date.getMonth = 5) {
+    return "May"
+} if (date.getMonth = 6) {
+    return "June"
+} if (date.getMonth = 7) {
+    return "July"
+} if (date.getMonth = 8) {
+    return "August"
+} if (date.getMonth = 9) {
+    return "September"
+} if (date.getMonth = 10) {
+    return "October"
+} if (date.getMonth = 11) {
+    return "November"
+} if (date.getMonth = 12) {
+    return "December"
+} 
+
 
 let todaysDate =
   date.getMonth() + 1 + "-" + date.getDate() + "-" + date.getFullYear();
