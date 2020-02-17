7903190ab27a3757aeb567e53b59cc824a41d5d6
Micah Lund <micah.r.lund@gmail.com>
2020-02-16 20:06:25 -0700

    Removed () to further problem solve.

--- main.js
+++ main.js
@@ -12,45 +12,46 @@ console.log(
 let month;
 
 //If or Switch statement for displaying the actual month name versus the month number.
-if ((date.getMonth() = 1)) {
+if ((date.getMonth = 1)) {
   month = "January";
 }
-if ((date.getMonth() = 2)) {
+if ((date.getMonth = 2)) {
   month = "February";
 }
-if ((date.getMonth() = 3)) {
+if ((date.getMonth = 3)) {
   month = "March";
 }
-if ((date.getMonth() = 4)) {
+if ((date.getMonth = 4)) {
   month = "April";
 }
-if ((date.getMonth() = 5)) {
+if ((date.getMonth = 5)) {
   month = "May";
 }
-if ((date.getMonth() = 6)) {
+if ((date.getMonth = 6)) {
   month = "June";
 }
-if ((date.getMonth() = 7)) {
+if ((date.getMonth = 7)) {
   month = "July";
 }
-if ((date.getMonth() = 8)) {
+if ((date.getMonth = 8)) {
   month = "August";
 }
-if ((date.getMonth() = 9)) {
+if ((date.getMonth = 9)) {
   month = "September";
 }
-if ((date.getMonth() = 10)) {
+if ((date.getMonth = 10)) {
   month = "October";
 }
-if ((date.getMonth() = 11)) {
+if ((date.getMonth = 11)) {
   month = "November";
 }
-if ((date.getMonth() = 12)) {
+if ((date.getMonth = 12)) {
   month = "December";
 }
 
 let todaysDate = month + " " + date.getDate() + ", " + date.getFullYear();
 console.log(todaysDate);
+console.log(date.getMonth());
 
 let newTD = document.getElementById("hw-date");
 newTD.innerText = todaysDate;
