417fe35b16af7bb105e168b5f3f277e06b028654
Micah Lund <micah.r.lund@gmail.com>
2020-02-18 20:58:32 -0700

    Decided that going down the path of using an if statement was a little messy. Going to try placing the month names in an array and pulling them that way.

--- main.js
+++ main.js
@@ -8,39 +8,14 @@ console.log(hw);
 console.log(
   date.getMonth() + 1 + " " + date.getDate() + " " + date.getFullYear()
 );
+console.log(date.getMonth() + 1);
 
-let month;
-
-//If or Switch statement for displaying the actual month name versus the month number. 
-if (date.getMonth = 1) {
-    month =  "January"
-} if (date.getMonth = 2) {
-    month = "February"
-} if (date.getMonth = 3) {
-    month = "March"
-} if (date.getMonth = 4) {
-    month = "April"
-} if (date.getMonth = 5) {
-    month = "May"
-} if (date.getMonth = 6) {
-    month = "June"
-} if (date.getMonth = 7) {
-    month = "July"
-} if (date.getMonth = 8) {
-    month = "August"
-} if (date.getMonth = 9) {
-    month = "September"
-} if (date.getMonth = 10) {
-    month = "October"
-} if (date.getMonth = 11) {
-    month = "November"
-} if (date.getMonth = 12) {
-    month = "December"
-} 
-
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 
 let todaysDate =
-  month + " " + date.getDate() + ", " + date.getFullYear();
+  monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
 console.log(todaysDate);
 
 let newTD = document.getElementById("hw-date");
