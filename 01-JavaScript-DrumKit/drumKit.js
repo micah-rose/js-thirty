04f6fde9ca728238241c834bd07b97161944446e
Micah Lund <micah.r.lund@gmail.com>
2020-03-10 21:56:13 -0600

    Console.log(audio) is not returning the audio element as expected in the video.

--- 01-JavaScript-DrumKit/drumKit.js
+++ 01-JavaScript-DrumKit/drumKit.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function (e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}]"`)
-    console.log(audio);
-})
+window.addEventListener("keydown", function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}]"`);
+  console.log(audio);
+});
