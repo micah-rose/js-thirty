fea033471501b2c45609d76078af2cc079dd2cb6
Micah Lund <micah.r.lund@gmail.com>
2020-03-15 18:58:46 -0600

    Added code to get the second hand to move based on a setInterval.

--- 02-JSandCSS-Clock/clock.js
+++ 02-JSandCSS-Clock/clock.js
@@ -1,1 +1,13 @@
 //JavaScript file for JS and CSS Clock
+
+const secondHand = document.querySelector(".second-hand");
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  console.log(seconds);
+}
+
+setInterval(setDate, 1000);
