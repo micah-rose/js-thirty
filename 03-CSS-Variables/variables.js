a9fb657a9f2652c7053b588f364df6e3627ee19a
Micah Lund <micah.r.lund@gmail.com>
2020-03-18 22:08:46 -0600

    Began writing JavaScript portion and event listeners.

--- 03-CSS-Variables/variables.js
+++ 03-CSS-Variables/variables.js
@@ -1,1 +1,9 @@
 //JavaScript file for CSS Variables
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate() {
+  console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
