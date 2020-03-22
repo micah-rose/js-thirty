890092717cba5b85d3b6d7d0efde5dbc58a34de0
Micah Lund <micah.r.lund@gmail.com>
2020-03-21 22:57:55 -0600

    Created a list of streets that contains de in the name, but I am receiving an uncaught error type message and the code will not work. Will need to troubleshoot this to determine what is wrong.

--- 04-ArrayCardio/cardio.js
+++ 04-ArrayCardio/cardio.js
@@ -82,6 +82,13 @@ const oldest = inventors.sort(function(a, b) {
 });
 console.table(oldest);
 
+//https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+const category = document.querySelector(".mw-category");
+const links = Array.from(category.querySelectorAll("a"));
+const de = links
+  .map(link => link.textContent)
+  .filter(streetName => streetName.includes("de"));
+
 const data = [
   "car",
   "car",
