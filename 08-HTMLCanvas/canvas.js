66e49563469e8bf634746ffcc58d461cc8780347
Micah Lund <micah.r.lund@gmail.com>
2020-04-04 23:16:12 -0600

    Added a direction variable and some additional if conditions to control the line width.

--- 08-HTMLCanvas/canvas.js
+++ 08-HTMLCanvas/canvas.js
@@ -12,12 +12,12 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
+let direction = true;
 
 function draw(e) {
   if (!isDrawing) return;
   console.log(e);
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-  ctx.lineWidth = hue;
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
@@ -27,6 +27,12 @@ function draw(e) {
   if (hue >= 360) {
     hue = 0;
   }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else ctx.lineWidth--;
 }
 
 canvas.addEventListener("mousemove", draw);
