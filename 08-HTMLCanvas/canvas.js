223ce28f42482d1c00ff63f315eaaa777356eef4
Micah Lund <micah.r.lund@gmail.com>
2020-04-04 22:45:02 -0600

    Added missing event listener for mouseout.

--- 08-HTMLCanvas/canvas.js
+++ 08-HTMLCanvas/canvas.js
@@ -19,3 +19,4 @@ function draw(e) {
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mousedown", () => (isDrawing = true));
 canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = true));
