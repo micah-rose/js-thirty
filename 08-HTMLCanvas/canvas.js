9bafdccd606e4bf06ce85e141c77e16c5f119a34
Micah Lund <micah.r.lund@gmail.com>
2020-04-04 22:36:04 -0600

    Created layout for HTML canvas.

--- 08-HTMLCanvas/canvas.js
+++ 08-HTMLCanvas/canvas.js
@@ -1,1 +1,8 @@
 //JS file for HTML Canvas exercise
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
