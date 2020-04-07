ac919b0d591ba8bb3ddad006f875033c3fe89bfb
Micah Lund <micah.r.lund@gmail.com>
2020-04-06 22:31:02 -0600

    Started working with a couple console.log usages.

--- 09-DevToolsDom/devTools.js
+++ 09-DevToolsDom/devTools.js
@@ -1,7 +1,17 @@
-const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+const dogs = [
+  { name: "Snickers", age: 2 },
+  { name: "hugo", age: 8 }
+];
 
 function makeGreen() {
-  const p = document.querySelector('p');
-  p.style.color = '#BADA55';
-  p.style.fontSize = '50px';
+  const p = document.querySelector("p");
+  p.style.color = "#BADA55";
+  p.style.fontSize = "50px";
 }
+
+//Different console.log usage
+//Regular
+console.log("hello");
+
+//Interpolated
+console.log("Hello I am a %s string!", "silly");
