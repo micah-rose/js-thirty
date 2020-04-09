bf12b23fd2a0425cd7d7ef3b1edb1febead4bc89
Micah Lund <micah.r.lund@gmail.com>
2020-04-08 23:11:15 -0600

    Set an if condition to log data to the console if a checkbox is clicked while holding down the shift key.

--- 10-HoldShift/holdShift.js
+++ 10-HoldShift/holdShift.js
@@ -3,7 +3,11 @@ const checkboxes = document.querySelectorAll('.inbox input[ type="checkbox"]');
 let lastChecked;
 
 function handleCheck(e) {
+  let inBetween = false;
   if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+    });
   }
   lastChecked = this;
 }
