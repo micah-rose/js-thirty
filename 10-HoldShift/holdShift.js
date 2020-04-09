09ee24f5dd352f3a6f0564847f60af748347320d
Micah Lund <micah.r.lund@gmail.com>
2020-04-08 23:14:37 -0600

    Set another if condition within a forEach loop to start checking the inbetween checkboxes from first checked to last checked.

--- 10-HoldShift/holdShift.js
+++ 10-HoldShift/holdShift.js
@@ -7,6 +7,10 @@ function handleCheck(e) {
   if (e.shiftKey && this.checked) {
     checkboxes.forEach(checkbox => {
       console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log("Starting to check them inbetwee!");
+      }
     });
   }
   lastChecked = this;
