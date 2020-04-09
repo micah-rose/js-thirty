ed0c44e130b07b8d50f71dbf1fbe6483983afacf
Micah Lund <micah.r.lund@gmail.com>
2020-04-08 23:07:01 -0600

    Added a lastChecked variable to use within our handleCheck function (handCLick was a mistake previously). Also began adding functionality to handleCheck.

--- 10-HoldShift/holdShift.js
+++ 10-HoldShift/holdShift.js
@@ -1,7 +1,11 @@
 const checkboxes = document.querySelectorAll('.inbox input[ type="checkbox"]');
 
+let lastChecked;
+
 function handleCheck(e) {
-  console.log(e);
+  if (e.shiftKey && this.checked) {
+  }
+  lastChecked = this;
 }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener("click", handleCheck));
