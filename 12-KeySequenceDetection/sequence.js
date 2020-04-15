e197505ca0520fd87ad1aeef97d09ff3d11432d8
Micah Lund <micah.r.lund@gmail.com>
2020-04-15 17:46:22 -0600

    Created an array for keys that are pressed and tied it into the event listener to push values into the array.

--- 12-KeySequenceDetection/sequence.js
+++ 12-KeySequenceDetection/sequence.js
@@ -1,4 +1,9 @@
 //JS file for Key Sequence Detection
+const pressed = [];
+const secretCode = "wesbos";
+
 window.addEventListener("keyup", e => {
   console.log(e.key);
+  pressed.push(e.key);
+  console.log(pressed);
 });
