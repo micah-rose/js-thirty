e933521659636ffb1766da41786eb476bc5405fe
Micah Lund <micah.r.lund@gmail.com>
2020-04-15 17:52:27 -0600

    finalized event listener with an if condition to send DING DING to the console when the secret code is created and to also display random items on the browser via cornify_add()

--- 12-KeySequenceDetection/sequence.js
+++ 12-KeySequenceDetection/sequence.js
@@ -4,7 +4,14 @@ const secretCode = "wesbos";
 
 window.addEventListener("keyup", e => {
   console.log(e.key);
+
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+  if (pressed.joined("").includes(secretCode)) {
+    console.log("DING DING");
+    cornify_add();
+  }
+
   console.log(pressed);
 });
