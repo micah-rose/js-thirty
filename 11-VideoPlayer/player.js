cbd9454bec8a838f46f69fb7410a3b16fa3e9101
Micah Lund <micah.r.lund@gmail.com>
2020-04-14 22:06:31 -0600

    Created JS elements.

--- 11-VideoPlayer/player.js
+++ 11-VideoPlayer/player.js
@@ -1,1 +1,8 @@
 //JS file for Video Player exercise
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
