4c4385dfd722e208f492595a5fd1d7365d7ec890
Micah Lund <micah.r.lund@gmail.com>
2020-04-17 19:48:12 -0600

    Started on slide scroll exercise and placed slider images into a variable to use in a checkSlide function.

--- 13-SlideInScroll/scroll.js
+++ 13-SlideInScroll/scroll.js
@@ -1,14 +1,23 @@
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
-  }
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide() {
+  console.log(e);
+}
+
+window.addEventListener("scroll", checkSlide);
