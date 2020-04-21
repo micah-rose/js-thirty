1e03bd340d1362242450fa8b53e2452197a8dc12
Micah Lund <micah.r.lund@gmail.com>
2020-04-21 17:29:33 -0600

    Added content for toggleDone function.

--- 15-LocalStorage/storage.js
+++ 15-LocalStorage/storage.js
@@ -31,6 +31,11 @@ function populateList(plates = [], plateList) {
     .join("");
 }
 
-function toggleDone(e){
-    
+function toggleDone(e) {
+  if (!e.target.matches("input")) return; // skip this unless it's an input
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
 }
