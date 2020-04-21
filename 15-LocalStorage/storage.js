0a88d51d60c78e445b9a43d04eaa3c00798fe1a6
Micah Lund <micah.r.lund@gmail.com>
2020-04-21 17:22:01 -0600

    Added content for addItem function and started populateList function that takes in two properties.

--- 15-LocalStorage/storage.js
+++ 15-LocalStorage/storage.js
@@ -2,4 +2,20 @@ const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = JSON.parse(localStorage.getItem("items")) || [];
 
-function addItem(e) {}
+function addItem(e) {
+  e.preventDefault();
+  const text = this.querySelector("[name=item]").value;
+  const item = {
+    text,
+    done: false
+  };
+
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
+  this.reset();
+}
+
+function populateList(plates = [], plateList){
+    
+}
