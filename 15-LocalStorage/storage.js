709ab139dd7cef1d6c74612c9497f0a463bea5cc
Micah Lund <micah.r.lund@gmail.com>
2020-04-21 17:20:12 -0600

    Started Local Storage exercise. Changed items array variable to include a JSON.parse and added an addItem function.

--- 15-LocalStorage/storage.js
+++ 15-LocalStorage/storage.js
@@ -1,3 +1,5 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
-const items = [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+function addItem(e) {}
