84e61ad5bd8cfcad1d16ed3fc13a017fd641a264
Micah Lund <micah.r.lund@gmail.com>
2020-04-24 16:45:53 -0600

    Added constant variables and chosen function to MouseMove exercise.

--- 16-MouseMove/mouse.js
+++ 16-MouseMove/mouse.js
@@ -1,1 +1,8 @@
 //JS file for Mouse Move Shadow
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 500; // 500px
+
+function shadow(e) {
+  console.log(e);
+}
