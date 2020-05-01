58c689139619ba669d72989702b2eb7c61632370
Micah Lund <micah.r.lund@gmail.com>
2020-05-01 16:49:08 -0600

    Stored a sort on the bands array in a sortedBands variable.

--- 17-SortWithoutArticles/sort.js
+++ 17-SortWithoutArticles/sort.js
@@ -17,3 +17,5 @@ const bands = [
 function strip(bandName) {
   return bandName.replace(/^(a |the |an )/i, "").trim();
 }
+
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
