070c0be66dbf7ba1e8fbd76efb2a1ad4189d7dd4
Micah Lund <micah.r.lund@gmail.com>
2020-05-03 21:11:28 -0600

    Added content to takePhoto function.

--- 19-Webcam/scripts.js
+++ 19-Webcam/scripts.js
@@ -48,7 +48,17 @@ function paintToCanvas() {
 }
 
 function takePhoto() {
-  console.log("Photo");
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild);
 }
 
 function redEffect(pixels) {
