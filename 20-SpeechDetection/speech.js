df71791dc2bf2885fba4bd8b92e459ff850bf63f
Micah Lund <micah.r.lund@gmail.com>
2020-05-08 17:39:54 -0600

    Added additional variables to Speech Detection.

--- 20-SpeechDetection/speech.js
+++ 20-SpeechDetection/speech.js
@@ -4,3 +4,7 @@ window.SpeechRecognition =
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 recognition.lang = "en-US";
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
