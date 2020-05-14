d902c2ecf97499b08ad9135f91d6c4b8ef82580d
Micah Lund <micah.r.lund@gmail.com>
2020-05-13 18:12:56 -0600

    Added a watchPosition function.

--- Geolocation/geo.js
+++ Geolocation/geo.js
@@ -1,3 +1,7 @@
 //JS file for Geolocation exercise
 const arrow = document.querySelector(".arrow");
 const speed = document.querySelector(".speed-value");
+
+navigator.geolocation.watchPosition((data) => {
+    console.log(data);
+}
