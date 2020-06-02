5eccc1fc35121030160fb6e80bd81c650b5b3f46
Micah Lund <micah.r.lund@gmail.com>
2020-06-01 20:31:10 -0600

    Added content to fixNav function and created event listener for user scrolling.

--- 24-StickyNav/sticky.js
+++ 24-StickyNav/sticky.js
@@ -3,5 +3,13 @@ const nav = document.querySelector("#main");
 let topOfNav = nav.offsetTop;
 
 function fixNav() {
-  console.log("Navigation is fixed");
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.classList.remove("fixed-nav");
+    document.body.style.paddingTop = 0;
+  }
 }
+
+window.addEventListener("scroll", fixNav);
