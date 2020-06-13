60a3b290b0fa99797183cf128b93eae4498140d6
Micah Lund <micah.r.lund@gmail.com>
2020-06-12 22:29:19 -0600

    Set variables triggers, background, and nav.

--- 26-StripeFollow/stripe.js
+++ 26-StripeFollow/stripe.js
@@ -1,1 +1,4 @@
 //JS File for Stripe Follow Along Nav exercise
+const triggers = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+const nav = document.querySelector(".top");
