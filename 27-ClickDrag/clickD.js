1c8f8afd4e43a546ea0229c3c7a637c0b1517ce0
Micah Lund <micah.r.lund@gmail.com>
2020-06-20 18:22:52 -0600

    Added second slider event listener for mouseleave.

--- 27-ClickDrag/clickD.js
+++ 27-ClickDrag/clickD.js
@@ -10,3 +10,8 @@ slider.addEventListener("mousedown", e => {
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
