a5f538c905be633884ae77be87d84afa643a13ef
Micah Lund <micah.r.lund@gmail.com>
2020-06-22 19:24:04 -0600

    Created final slider event listener for mousemove action.

--- 27-ClickDrag/clickD.js
+++ 27-ClickDrag/clickD.js
@@ -20,3 +20,11 @@ slider.addEventListener("mouseup", () => {
   isDown = false;
   slider.classList.remove("active");
 });
+
+slider.addEventListener("mousemove", e => {
+  if (!isDown) return; // stop the fn from running
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+});
